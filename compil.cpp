4cb9ae95288ea30cf2c86e9b0c75b9e0626b8620
SashaKhaytina <sasha-khaytina@rambler.ru>
2024-10-17 21:11:37 +0300

    cucle (registrs (counter)) in assembler (compil) and in processor (program_asm -> program_code)

--- compil.cpp
+++ compil.cpp
@@ -1,15 +1,38 @@
+// Порядок запуска:
+// g++ compil.cpp
+// ./a.out
+// g++ processor.cpp Stack/stack_commands.cpp Stack/errors.cpp Stack/utils.cpp -D_DEBUG -ggdb3 -std=c++17 -O0 -Wall -Wextra -Weffc++ -Waggressive-loop-optimizations -Wc++14-compat -Wmissing-declarations -Wcast-align -Wcast-qual -Wchar-subscripts -Wconditionally-supported -Wconversion -Wctor-dtor-privacy -Wempty-body -Wfloat-equal -Wformat-nonliteral -Wformat-security -Wformat-signedness -Wformat=2 -Winline -Wlogical-op -Wnon-virtual-dtor -Wopenmp-simd -Woverloaded-virtual -Wpacked -Wpointer-arith -Winit-self -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=2 -Wsuggest-attribute=noreturn -Wsuggest-final-methods -Wsuggest-final-types -Wsuggest-override -Wswitch-default -Wswitch-enum -Wsync-nand -Wundef -Wunreachable-code -Wunused -Wuseless-cast -Wvariadic-macros -Wno-literal-suffix -Wno-missing-field-initializers -Wno-narrowing -Wno-old-style-cast -Wno-varargs -Wstack-protector -fcheck-new -fsized-deallocation -fstack-protector -fstrict-overflow -flto-odr-type-merging -fno-omit-frame-pointer -Wlarger-than=8000 -Wstack-usage=8192 -pie -fPIE -Werror=vla -fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr -o processor
+// ./processor
+
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // Должен переводить файл слов (асемблер) в файл с маш кодом (файл с цифрами)
+
+enum IndexRegistrs 
+{
+    RAX,
+    RBX,
+    RCX, 
+    RDX,
+    REX,
+};
+
 enum MashineCode
 {
     HLT = 0,
     PUSH = 51,
+    PUSHR = 52,
+    POP = 42,
     ADD = 1,
     SUB = 2,
     MUL = 3, 
-    OUT = 4 // ыыыы
+    OUT = 4, // ыыыы
+    JUMP = 5,
+    JA = 6
 };
 
 void run_compil(int argc, const char *argv[]);
@@ -54,17 +77,61 @@ void run_compil(int argc, const char *argv[])
         {
             //printf("asdaslkdnaishfasgyusdyugiuguyzgdfuigyzrgiuzgtyi\n");
             // printf("%d ", PUSH);
-            fprintf(file_code, "%d ", PUSH);
 
-            double arg = 0;
-            fscanf(file_asm, "%lg", &arg);
-            fprintf(file_code, "%lg \n", arg);
+            //fprintf(file_code, "%d ", PUSH);
+
+            // double arg = 0;
+            // fscanf(file_asm, "%lg", &arg);
+            // fprintf(file_code, "%lg \n", arg);
+
+            char arg[10] = {};
+            fscanf(file_asm, "%s", arg);
+            if (isdigit(arg[0]))
+            {
+                fprintf(file_code, "%d ", PUSH);
+                fprintf(file_code, "%lg \n", (double) atoi(arg));
+            }
+            else
+            {
+                fprintf(file_code, "%d ", PUSHR);
+
+                if      (strcmp(arg, "RAX") == 0) fprintf(file_code, "%d \n", RAX); // ТУТ ДОЛЖЕН БЫТЬ if С ПРОВЕРКОЙ, что это за регистр
+                else if (strcmp(arg, "RBX") == 0) fprintf(file_code, "%d \n", RBX);
+                else if (strcmp(arg, "RCX") == 0) fprintf(file_code, "%d \n", RCX);
+                else if (strcmp(arg, "RDX") == 0) fprintf(file_code, "%d \n", RDX);
+                else if (strcmp(arg, "REX") == 0) fprintf(file_code, "%d \n", REX);
+            }
+
+
+
+
             // printf("%lg ", arg);
 
             //counter_comand++;
 
             continue;
-        } 
+        }
+
+        if (strcmp(command, "POP") == 0)
+        {
+            fprintf(file_code, "%d ", POP);
+
+            char arg[10] = {};
+            fscanf(file_asm, "%s", arg);
+
+            //fprintf(file_code, "0 \n"); // ТУТ ДОЛЖЕН БЫТЬ if С ПРОВЕРКОЙ arg, что это за регистр
+            if      (strcmp(arg, "RAX") == 0) fprintf(file_code, "%d \n", RAX); // ТУТ ДОЛЖЕН БЫТЬ if С ПРОВЕРКОЙ, что это за регистр
+            else if (strcmp(arg, "RBX") == 0) fprintf(file_code, "%d \n", RBX);
+            else if (strcmp(arg, "RCX") == 0) fprintf(file_code, "%d \n", RCX);
+            else if (strcmp(arg, "RDX") == 0) fprintf(file_code, "%d \n", RDX);
+            else if (strcmp(arg, "REX") == 0) fprintf(file_code, "%d \n", REX);
+
+            //counter_comand++;
+
+            continue;
+        }
+
+        
 
         if (strcmp(command, "ADD") == 0)
         {
@@ -96,6 +163,32 @@ void run_compil(int argc, const char *argv[])
         if (strcmp(command, "OUT") == 0)
         {
             fprintf(file_code, "%d \n", OUT);
+
+            //counter_comand++;
+
+            continue;
+        }
+
+        if (strcmp(command, "JUMP") == 0)
+        {
+            fprintf(file_code, "%d ", JUMP);
+
+            int arg = 0;
+            fscanf(file_asm, "%d", &arg);
+            fprintf(file_code, "%d \n", arg);
+
+            //counter_comand++;
+
+            continue;
+        }
+
+        if (strcmp(command, "JA") == 0)
+        {
+            fprintf(file_code, "%d ", JA);
+
+            int arg = 0;
+            fscanf(file_asm, "%d", &arg);
+            fprintf(file_code, "%d \n", arg);
 
             //counter_comand++;
 
